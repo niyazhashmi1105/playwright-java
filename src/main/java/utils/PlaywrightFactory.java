1c227da3d586739bdb5d3a35c3f1f5e6e539658e
MD Niyaz Hashmi <mdniyazhashmi@192.168.1.2>
2024-10-19 02:56:36 +0530

    added docker compose

--- src/main/java/utils/PlaywrightFactory.java
+++ src/main/java/utils/PlaywrightFactory.java
@@ -10,31 +10,34 @@ public class PlaywrightFactory {
     Playwright playwright;
     Browser browser;
     Page page;
-    ConfigReader configReader;
 
     public PlaywrightFactory(){
-        configReader = new ConfigReader();
+        new ConfigReader();
     }
 
     public Page initBrowser(){
-        String browserType = ConfigReader.getProperty("browser");
 
+        String browserType = ConfigReader.getProperty("browser");
         playwright = Playwright.create();
 
-        switch(browserType.toLowerCase()){
+            switch (browserType.toLowerCase()) {
 
-            case "chrome":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-                break;
-            case "firefox":
-                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
-                break;
-            case "webkit":
-                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(true));
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browserType);
-        }
+                case "chrome":
+                    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(ConfigReader.getProperty("headless"))).setSlowMo(Double.parseDouble(ConfigReader.getProperty("slow.motion"))));
+                    break;
+                case "firefox":
+                    browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(ConfigReader.getProperty("headless"))).setSlowMo(Double.parseDouble(ConfigReader.getProperty("slow.motion"))));
+                    break;
+                case "webkit":
+                    browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(ConfigReader.getProperty("headless"))).setSlowMo(Double.parseDouble(ConfigReader.getProperty("slow.motion"))));
+                    break;
+                case "edge":
+                    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge").setHeadless(Boolean.parseBoolean(ConfigReader.getProperty("headless"))).setSlowMo(Double.parseDouble(ConfigReader.getProperty("slow.motion"))));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported browser: " + browserType);
+            }
+
         page = browser.newPage();
 //        int width=1710; //Macbook only
 //        int height = 993; //Macbook only
